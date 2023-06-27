a88f66bbabaf306322b44aa1d033efe86712faec
DanielaEstefany <estefanymunoz0@gmail.com>
2023-06-27 04:07:38 -0400

    terminado1

--- game.py
+++ game.py
@@ -12,42 +12,31 @@ class Game:
         self.height = height
 
         # Resto de la inicialización del juego
-        self.mapa = pygame.image.load("mapa.png")  # Reemplaza "nombre_del_archivo.png" con el nombre de tu imagen de mapa
+        self.mapa = pygame.image.load("mapa1.png")  # Reemplaza "nombre_del_archivo.png" con el nombre de tu imagen de mapa
         self.mapa = pygame.transform.scale(self.mapa, (self.width, self.height))
 
-        self.blocked_blocks = (
-            [(1, col) for col in range(1, 26)] +
-            [(2, col) for col in range(1, 3)] + [(2, col) for col in range(5, 11)] + [(2, col) for col in range(13, 22)] + [(2, col) for col in range(24, 26)] +
-            [(3, col) for col in range(1, 3)] + [(3, col) for col in range(5, 11)] + [(3, col) for col in range(13, 22)] + [(3, col) for col in range(24, 26)] +
-            [(4, 1)]+ [(4, 3)] + [(4, 5)]+ [(4, 7)] + [(4, 9)]+ [(4, 10)] + [(4, 13)]+ [(4, 15)] + [(4, 16)]+ [(4, 3)] +
-            [(10, col) for col in range(1, 3)] +
-            [(11, col) for col in range(1, 3)] +
-            [(12, 1)]+ [(12, 3)]+
-            [(14, col) for col in range(1, 3)] + [(14, col) for col in range(5, 6)] + [(14, col) for col in range(8, 11)]
-        )
-
         self.matrizG = [
-        ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#'],
-        ['#', '1', '#', ' ', '#', '2', '#', '3', '#', '#', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', ' ', '#', '#', ' ', ' ', '#', ' ', '#'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', ' ', '#'],
-        ['#', 'E', '#', ' ', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        [' ', ' ', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' '],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', ' ', 'U', '#', ' ', '#', '#', '#', '#'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#'],
-        ['#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', ' ', ' ', ' ', '#', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#'],
-        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],    
+        ['#', '#', '#', 'K', '#', '#', '#', '#', '#', '#', '#', 'N', 'N', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'R', '#', '#', '#'],
+        ['#', '#', '#', 'K', '#', '#', '#', '#', '#', '#', '#', 'N', 'N', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'R', '#', '#', '#'],
+        ['#', '1', '#', 'K', '#', '2', '#', '3', '#', '#', '4', 'N', 'N', '5', '#', '#', '6', '#', '7', '#', '#', '8', 'R', '#', '9', '#'],
+        ['A', 'A', 'A','AK', 'A', 'A', 'A', 'A', 'A', 'A', 'A','AN','BN', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'BR','B', 'B', 'B'],
+        ['#', '#', '#', 'K', '#', '#', '#', '#', '#', '#', '#', 'N', 'N', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'R', '#', '#', '#'],
+        ['#', '#', '#', 'K', '#', '11', '#','12','#', '13','#', 'N', 'N', '#', '14','#', '15','#','16', '#', '17','#', 'R', '#', '18','#'],
+        ['#','10', '#', 'K', '#', '#','CM', 'C', 'C', 'C', 'C','CN','DN', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D','DR', 'D','DS', 'D'],
+        ['Z', 'Z', 'Z', 'K', '19','#', 'M', '#', '#', '20','#', 'N', 'N', '#', '21','#', '#', '#', '#', '#', '#', '#', '#', '#', 'S', 'S'],
+        ['#', '#', '#', 'K ','#', '#', 'M', '#', '#', '#', '#', 'N', 'N', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'S', 'S'],
+        ['#', '#', '#', 'K', '#', '#', 'M', '#', '#', '#', '#', 'N', 'N', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'S', 'S'],
+        ['#', '22','#', 'K', '23','#', 'M', '24','#', '25','NE','N', 'N','NE', '#', '26','#','27', '#','28', '#', '#','29', '#', 'S', '#'],
+        ['E', 'E', 'E','EK', 'E', 'E','EM', 'E', 'E', 'E', 'E', '30','#', 'E', 'F', 'F', 'F', 'F','FP', 'F', 'F', 'F', 'F', 'F','FS','31'],
+        ['#', '#', '#', 'L', '#', '#', 'M', '#', '#', '#','OE', 'O', 'O','OE', '#', '#', '#', '#', 'P', '#', '#', 'Q', '#', '#', '#', ' '],
+        ['#', '#', '#', 'L', '#', '#', 'M', '#', '#', '#', '#', 'O', 'O', '#', '#', '#', '#', '#', 'P', '#', '#', 'Q', '#', '#', '#', '#'],
+        ['#', '#', '#', 'L', '#', '#', 'M', '#', '#', '#', '#', 'O', 'O', '#', '#', '#', '#', '#', 'P', '#', '#', 'Q', '#', '#', '#', '#'],
+        ['#', '#', '32','L','33', '#', 'M', '#','34', '#','35', 'O', 'O', '#', '#', '#','36', '#', 'P','37', '#', 'Q','38', '#','39', '#'],
+        ['G', 'G', 'G','GL', 'G', 'G','GM', 'G', 'G', 'G', 'G','GO','GO', 'V', 'V', 'V', 'V', 'V','HP', 'H', 'H','HQ', 'H', 'H', 'H', 'H'],
+        ['#', '#', '#', 'L', '#', '#', 'M', '#', '#', '#', '#', 'O', 'O', '#', '#', '#', '#', '#', 'P', '#', '#', 'Q', '#', '#', '#', '#'],
+        ['#', '40','#', 'L','41', '#', 'M', '#', '#','42', '#', 'O', 'O', '#','43', '#','44', '#', 'P', '#','45', 'Q', '#','46', '#','47'],
+        ['I', 'I', 'I','IL', 'I', 'I','IM', 'I', 'I', 'I', 'I','IO','IO', 'V', 'V', 'V', 'V', 'V', 'P', 'J', 'J','JQ', 'J', 'J', 'J', 'J']
         ]
 
         self.block_width = self.width // 26
@@ -133,6 +122,8 @@ class Game:
             pygame.time.wait(200)  # Pausa de 200 milisegundos entre cada movimiento del robot
 
     def render(self):
+        imagen_robot = pygame.image.load("gordo.png")
+        imagen_robot = pygame.transform.scale(imagen_robot, (self.block_width, self.block_height))  # Ajustar tamaño de la imagen al bloque
         # Limpiar la ventana con un color de fondo
         self.window.fill((255, 255, 255))
 
@@ -150,26 +141,225 @@ class Game:
             pygame.draw.line(self.window, (0, 0, 0), (0, y), (self.width, y))
 
         # Dibujar el robot en la posición actual
-        robot_x = (self.robot_block[1] - 1) * self.block_width + self.block_width // 2
-        robot_y = (self.robot_block[0] - 1) * self.block_height + self.block_height // 2
-        pygame.draw.circle(self.window, (255, 0, 0), (robot_x, robot_y), min(self.block_width, self.block_height) // 2)
+        robot_x = (self.robot_block[1] - 1) * self.block_width
+        robot_y = (self.robot_block[0] - 1) * self.block_height
+        self.window.blit(imagen_robot, (robot_x, robot_y))
 
         # Actualizar la ventana
         pygame.display.flip()
 
     def process_text(self, text):
-        if "escuela" in text.lower():
-            self.movement_requested = "escuela"
-        elif "universidad" in text.lower():
-            self.movement_requested = "universidad"
-        elif "circo" in text.lower():
-            self.movement_requested = "circo"
-        elif "calle 1" in text.lower():
-            self.movement_requested = "calle 1"
-        elif "calle 2" in text.lower():
-            self.movement_requested = "calle 2"
-        elif "pizza" in text.lower():
-            self.movement_requested = "pizza"
+
+        frase1 = "gordo ve a"
+        frase2 = "gordo muevete a"
+        frase3 = "gordo dirigete a"
+        frase4 = "gordo conduce a"
+        frase5 = "gordo trasladate a"
+        frases = [frase1, frase2, frase3, frase4, frase5]
+
+        for frase in frases:
+            if frase in text.lower():
+                if "jugueteria" in text.lower():
+                    self.movement_requested = "jugueteria"
+                    break
+                elif "lavanderia" in text.lower():
+                    self.movement_requested = "lavanderia"
+                    break
+                elif "fruteria" in text.lower():
+                    self.movement_requested = "fruteria"
+                    break
+                elif "supermercado" in text.lower():
+                    self.movement_requested = "supermercado"
+                    break
+                elif "restaurante" in text.lower():
+                    self.movement_requested = "restaurante"
+                    break
+                elif "pescadería" in text.lower():
+                    self.movement_requested = "pescadería"
+                    break
+                elif "veterinario" in text.lower():
+                    self.movement_requested = "veterinario"
+                    break
+                elif "gasolinera" in text.lower():
+                    self.movement_requested = "gasolinera"
+                    break
+                elif "escuela" in text.lower():
+                    self.movement_requested = "escuela"
+                    break
+                elif "carnicería" in text.lower():
+                    self.movement_requested = "carnicería"
+                    break
+                elif "tienda de instrumentos" in text.lower():
+                    self.movement_requested = "tienda de instrumentos"
+                    break
+                elif "librería" in text.lower():
+                    self.movement_requested = "librería"
+                    break
+                elif "sala de conciertos" in text.lower():
+                    self.movement_requested = "sala de conciertos"
+                    break
+
+                elif "cine" in text.lower():
+                    self.movement_requested = "cine"
+                    break
+                elif "quiosco" in text.lower():
+                    self.movement_requested = "quiosco"
+                    break
+                elif "academia de idiomas" in text.lower():
+                    self.movement_requested = "academia de idiomas"
+                    break
+                elif "pizzería" in text.lower():
+                    self.movement_requested = "pizzería"
+                    break
+                elif "aparcamiento" in text.lower():
+                    self.movement_requested = "aparcamiento"
+                    break
+                elif "iglesia" in text.lower():
+                    self.movement_requested = "iglesia"
+                    break
+                elif "ayuntamiento" in text.lower():
+                    self.movement_requested = "ayuntamiento"
+                    break
+                elif "cafetería" in text.lower():
+                    self.movement_requested = "cafetería"
+                    break
+                elif "herboristería" in text.lower():
+                    self.movement_requested = "herboristería"
+                    break
+                elif "correos" in text.lower():
+                    self.movement_requested = "correos"
+                    break
+                elif "parada de autobús" in text.lower():
+                    self.movement_requested = "parada de autobús"
+                    break
+                elif "banco" in text.lower():
+                    self.movement_requested = "banco"
+                    break
+                elif "embajada" in text.lower():
+                    self.movement_requested = "embajada"
+                    break
+                elif "hotel" in text.lower():
+                    self.movement_requested = "hotel"
+                    break
+                elif "comisaria de policía" in text.lower():
+                    self.movement_requested = "comisaria de policía"
+                    break
+                elif "plaza" in text.lower():
+                    self.movement_requested = "plaza"
+                    break
+                elif "monumento nivel" in text.lower():
+                    self.movement_requested = "monumento nivel"
+                    break
+                elif "estación de bomberos" in text.lower():
+                    self.movement_requested = "estación de bomberos"
+                    break
+                elif "museo" in text.lower():
+                    self.movement_requested = "museo"
+                    break
+                elif "hospital" in text.lower():
+                    self.movement_requested = "hospital"
+                    break
+                elif "floristería" in text.lower():
+                    self.movement_requested = "floristería"
+                    break
+                elif "biblioteca" in text.lower():
+                    self.movement_requested = "biblioteca"
+                    break
+                elif "universidad" in text.lower():
+                    self.movement_requested = "universidad"
+                    break
+                elif "bar" in text.lower():
+                    self.movement_requested = "bar"
+                    break
+                elif "estación de tren" in text.lower():
+                    self.movement_requested = "estación de tren"
+                    break
+                elif "peluquería" in text.lower():
+                    self.movement_requested = "peluquería"
+                    break
+                elif "centro comercial" in text.lower():
+                    self.movement_requested = "centro comercial"
+                    break
+                elif "farmacia" in text.lower():
+                    self.movement_requested = "farmacia"
+                    break
+                elif "circo" in text.lower():
+                    self.movement_requested = "circo"
+                    break
+                elif "teatro" in text.lower():
+                    self.movement_requested = "teatro"
+                    break
+                elif "tienda de ropa" in text.lower():
+                    self.movement_requested = "tienda de ropa"
+                    break
+                elif "casa de pepe" in text.lower():
+                    self.movement_requested = "casa de pepe"
+                    break
+                elif "ambulatorio" in text.lower():
+                    self.movement_requested = "ambulatorio"
+                    break
+                elif "calle profe inolvidable" in text.lower():
+                    self.movement_requested = "calle profe inolvidable"
+                    break
+                elif "calle del vocabulario" in text.lower():
+                    self.movement_requested = "calle del vocabulario"
+                    break
+                elif "calle del ser y estar" in text.lower():
+                    self.movement_requested = "calle del ser y estar"
+                    break
+                elif "calle del instituto cervantes" in text.lower():
+                    self.movement_requested = "calle del instituto cervantes"
+                    break
+                elif "avenida hablo español" in text.lower():
+                    self.movement_requested = "avenida hablo español"
+                    break
+                elif "avenida porfe dele" in text.lower():
+                    self.movement_requested = "avenida porfe dele"
+                    break
+                elif "calle del sustantivo" in text.lower():
+                    self.movement_requested = "calle del sustantivo"
+                    break
+                elif "calle del me gusta" in text.lower():
+                    self.movement_requested = "calle del me gusta"
+                    break
+                elif "calle de los errores" in text.lower():
+                    self.movement_requested = "calle de los errores"
+                    break
+                elif "calle de por y para" in text.lower():
+                    self.movement_requested = "calle de por y para"
+                    breakpoint
+                elif "calle del adjetivo" in text.lower():
+                    self.movement_requested = "calle del adjetivo"
+                    break
+                elif "calle del siele" in text.lower():
+                    self.movement_requested = "calle del siele"
+                    break
+                elif "calle de los deberes hechos" in text.lower():
+                    self.movement_requested = "calle de los deberes hechos"
+                    break
+                elif "avenida del subjuntivo" in text.lower():
+                    self.movement_requested = "avenida del subjuntivo"
+                    break
+                elif "avenida del indicativo" in text.lower():
+                    self.movement_requested = "avenida del indicativo"
+                    break
+                elif "calle de los verbos" in text.lower():
+                    self.movement_requested = "calle de los verbos"
+                    break
+                elif "calle de la gramatica" in text.lower():
+                    self.movement_requested = "calle de la gramatica"
+                    break
+                elif "calle de las dudas" in text.lower():
+                    self.movement_requested = "calle de las dudas"
+                    break
+                elif "calle de la n" in text.lower():
+                    self.movement_requested = "calle de la n"
+                    break       
+                elif " " in text.lower():
+                    print("no conozco ese lugar")
+
+        else:
+            print("no se hacer eso xd")
         
 
     def crearMatriz(self, x, y):
@@ -187,7 +377,7 @@ class Game:
 
     def move_robot_to_school(self):
         # Calcular la dirección del movimiento hacia la escuela
-        school = 'E'
+        school = '10'
         robotPos = (self.robot_block[0]-1,self.robot_block[1]-1)
         caminoPos = [(self.robot_block[0]-1,self.robot_block[1]-1)]
         camino = self.buscar_queso(self.matrizG, robotPos, caminoPos, school)
